a8b28a089c9dca332aabdb07dc78f9daeaca14b4
Ethan Allen-Harris <ethanaharris10@gmail.com>
2023-03-06 16:00:02 +0000

    begun set reminder intent_handler + comments

--- __init__.py
+++ __init__.py
@@ -1,4 +1,6 @@
 from mycroft import MycroftSkill, intent_handler
+from mycroft.util.parse import extract_datetime, normalize
+from mycroft.util.format import nice_time, nice_date
 
 
 class RoutineNew(MycroftSkill):
@@ -14,20 +16,146 @@ class RoutineNew(MycroftSkill):
     def handle_routine_set(self, message):
         #Needs to set routine here
 
-        #Check if user gave a routine
-        routine = message.data.get('routine')
+        #Check if user gave a routine activity
+        activity = message.data.get('routine')
 
 
         #appends to list of routines
 
-        self.settings['routine'] = [(routine, '9 AM', 'everyday')]
+        #has set routine
+        
+        #maybe ask user if they are sure they want to add that to their routine
+        
+        
+        
+        
+        #SAMPLE CONVERSATION
+        
+        #"can you add a routine {routine}"
+        
+        #"sure, I can add {routine} for 9 AM everyday"
+        
+        #"would you like to change the date or time of this routine, respond with 'yes' or 'no'"
+        #**Listens**
+        
+        #"yes"
+        
+        #"alright what time or day would you like for this activity to be in your routine?"
+        #(maybe give the user a sample response)
+        
+        #"nine thirty, monday and thursday"
+        
+        #**will disect response and re-arrange routine time/days**
+        
+        
+        #see if user can add a time or date to routine
+        
+        
+        
+        
+        #User calls intent
+        
+        #Check user actually has an activity
+        
+        #If no, say "please provide an activity to your routine"
+        #End
+        
+        
+        #if user has an activity
+        #output dialog
+        #then ask if user would like to change date / time
+        
+        if activity == "":
+            #No routine activity supplied
+            #NEED DIALOG FILE
+            self.speak_dialog('Please supply an activity for your routine')
+            
+        else:
+            #If user did supply activity
+            
+            #say to user "I have created activity for 9 AM everyday"
+            self.speak_dialog('routine.set', data={
+            'routine': activity,
+            'time': '12 AM',
+            'days': 'everyday'})
+            
+            
+            #expect yes/no response
+            #NEED DIALOG FILE
+            if self.ask_yesno('would you like to change the date or for ' + activity) == 'yes':
+                #if user wants to change time/date of routine
+                
+                #Ask what time the user would like for this activity
+                #NEED DIALOG FILE
+                response = self.get_response('What time do you want for ' + activity)
+                
+                reminder_time, rest = (extract_datetime(response, now_local(), self.lang, default_time=DEFAULT_TIME) or (None, None))
+                
+                self.speak_dialog(reminder_time)
+                
+                                                                                    
+                #response
+                
+                #handle "twelve thirty pm" and convert into time
+                
+                
+                
+                
+                
+                
 
-
+                #if invalid answer say "Activity has been set for 12 AM, are you happy with this?"
+                #yes/no
+                
+                
+                
+                #if no loop, and ask user for what time they would like
+                
+                
+                #-------------------------------------------------------------------------------
+                
+                #Ask what days the user would like for this activity
+                
+                
+                
+                
+                
+                #if invalid answer say "Activity has been set for 12 AM, are you happy with this?"
+                #yesno
+                
+                #if 'no' loop, and ask user for what days they would like
+                
+                #if 'yes' go to days
+                #--------------------------------------------------------------------------------
+                
+                #set routine
+                
+                #say "alright I have created a routine for x at time everyday"
+                
+                
+                
+                
+            
+            
+           
+           
+           
+            
         #Says to user "alright I have created a routine for x at time everyday"
-        self.speak_dialog('routine.set', data={
-        'routine': routine,
-        'time': '9 AM',
-        'days': 'everyday'})
+        #self.speak_dialog('routine.set', data={'routine': activity,'time': '9 AM','days': 'everyday'})
+        
+        
+        
+        #empties directory
+        self.settings['routine'] = [];
+        
+        #if user is cool with default time
+        
+        #will store data into a directory
+        self.settings['routine'].append((activity, '12 AM', 'everyday'))
+        
+        
+
         
         
     @intent_handler('routine.change.intent')
